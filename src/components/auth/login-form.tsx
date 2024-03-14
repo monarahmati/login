2706617740e5f6c9f644d8d4a5c988addbf051a4
monarahmati <monarahmati.official@gmail.com>
2024-03-15 00:34:17 +0330

    new commit

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -6,9 +6,26 @@ import * as yup from "yup";
 import { loginConfig } from "../../config/features/auth/auth-config";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { IconButton, InputAdornment } from "@mui/material";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
+
 
 
 const loginForm = () => {
+
+
+
+  //PASSWORD_STATE
+  const [showPasword, setShowPassword] = useState(false);
+
+  //TOGGLE_SEE_PASSWORD
+  const toggleSeePassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   //REQUIRED-WITH-YUP
   const loginFormSchema = yup.object({
     [loginConfig.username]: yup.string().required(),
@@ -53,8 +70,37 @@ const loginForm = () => {
                 label="نام کاربری"
                 variant="outlined"
                 {...register(loginConfig.username)}
-                error={!!errors[loginConfig.username] }
-                helperText={(errors[loginConfig.username]?.message || "") as any }
+                error={!!errors[loginConfig.username]}
+                helperText={
+                  (errors[loginConfig.username]?.message || "") as any
+                }
+                fullWidth
+                autoComplete="off"
+              />
+
+              <TextField
+                id="password-input"
+                label="رمز ورود"
+                variant="outlined"
+                {...register(loginConfig.password)}
+                error={!!errors[loginConfig.password]}
+                helperText={
+                  (errors[loginConfig.password]?.message || "") as any
+                }
+                fullWidth
+                type={showPasword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={toggleSeePassword}>
+                        {
+                            showPasword ? ( <RemoveRedEyeIcon/> ) : (<VisibilityOffIcon/> )
+                        }
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                autoComplete="off"
               />
             </Stack>
           </Stack>
